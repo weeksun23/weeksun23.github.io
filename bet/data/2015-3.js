28bdd2593dde66a5159bcdd4c6cdb0aea92ec167
Lich <jy013018@aliyun.com>
2015-03-03 11:23:28 +0800

    更新bet

--- bet/data/2015-3.js
+++ bet/data/2015-3.js
@@ -1,39 +1,81 @@
 var data = [{
+    /**************************0301***************************/
     type : '意甲',time : '0301 22:00',money : 30,reward : 0,
     items : [
-        {host : '巴勒莫',visiting : '恩波利',play : '巴勒莫-0.5',odds : 2.3}
+        {host : '巴勒莫',visiting : '恩波利',play : '巴勒莫-0.5',odds : 2.3,result : '输'}
     ]
 },{
 	type : 'NBA',time : '0301 22:30',money : 30,reward : 57.28,
     items : [
-        {host : '黄蜂',visiting : '魔术',play : '小187.0',odds : 1.9}
+        {host : '黄蜂',visiting : '魔术',play : '小187.0',odds : 1.9,result : '赢'}
     ]
 },{
 	type : 'NBA',time : '0301 22:47',money : 20,reward : 0,
     items : [
-        {host : '76人',visiting : '步行者',play : '大183.5',odds : 1.47},
-        {host : '鹈鹕',visiting : '掘金',play : '小205.0',odds : 1.47},
-        {host : '黄蜂',visiting : '魔术',play : '小189.0',odds : 1.74}
+        {host : '76人',visiting : '步行者',play : '大183.5',odds : 1.47,result : '输'},
+        {host : '鹈鹕',visiting : '掘金',play : '小205.0',odds : 1.47,result : '赢'},
+        {host : '黄蜂',visiting : '魔术',play : '小189.0',odds : 1.74,result : '赢'}
     ]
 },{
+    /**************************0302***************************/
 	type : 'NBA',time : '0302 04:39',money : 40,reward : 74.79,
     items : [
-        {host : '骑士',visiting : '火箭',play : '火箭+1.5',odds : 1.86}
+        {host : '骑士',visiting : '火箭',play : '火箭+1.5',odds : 1.86,result : '赢'}
     ]
 },{
 	type : '韩篮甲',time : '0302 11:21',money : 15,reward : 0,
     items : [
-        {host : '仁川大象',visiting : '蔚山现代太阳神',play : '大145.0',odds : 1.44},
-        {host : '昌原猎隼',visiting : '首尔迅雷',play : '大154.5',odds : 1.49}
+        {host : '仁川大象',visiting : '蔚山现代太阳神',play : '大145.0',odds : 1.44,result : '输'},
+        {host : '昌原猎隼',visiting : '首尔迅雷',play : '大154.5',odds : 1.49,result : '赢'}
     ]
 },{
-	type : 'NCAA',time : '0302 11:23',money : 20,reward : 0,
+	type : 'NCAA滚',time : '0302 11:23',money : 20,reward : 0,
 	items : [
-        {host : '华盛顿州',visiting : 'UCLA',play : '大143.5',odds : 1.86}
+        {host : '华盛顿州',visiting : 'UCLA',play : '大143.5',odds : 1.86,result : '输'}
     ]
 },{
 	type : '韩篮甲',time : '0302 17:32',money : 30,reward : 0,
     items : [
-        {host : '仁川大象',visiting : '蔚山现代太阳神',play : '大151.0',odds : 1.9}
+        {host : '仁川大象',visiting : '蔚山现代太阳神',play : '大151.0',odds : 1.9,result : '输'}
+    ]
+},{
+    type : 'NBA',time : '0302 22:44',money : 30,reward : 0,
+    items : [
+        {host : '篮网',visiting : '勇士',play : '小207.0',odds : 1.9,result : '输'}
+    ]
+},{
+    type : 'NBA',time : '0302 22:52',money : 10,reward : 0,
+    items : [
+        {host : '76人',visiting : '猛龙',play : '小193.0',odds : 1.9,result : '输'},
+        {host : '篮网',visiting : '勇士',play : '小207.0',odds : 1.9,result : '输'}
+    ]
+},{
+    type : "意甲德乙",time : '0302 23:02',money : 20,reward : 0,
+    items : [
+        {host : '罗马',visiting : '尤文图斯',play : '罗马+0.5',odds : 1.5,result : '赢'},
+        {host : '因戈尔施塔特',visiting : '慕尼黑1860',play : '因戈尔施塔特-0.5',odds : 1.45,result : '输'}
+    ]
+},{
+    type : '土超',time : '0303 00:08',money : 20,reward : 36.50,
+    items : [
+        {host : '特拉布宗体育',visiting : '卡拉布克士邦',play : '特拉布宗体育-0.5',odds : 1.825,result : '赢'}
+    ]
+},{
+    type : '西甲',time : '0303 00:08',money : 20,reward : 39.50,
+    items : [
+        {host : '塞尔塔',visiting : '艾尔切',play : '特拉布宗体育-0.5',odds : 1.975,result : '赢'}
+    ]
+},{
+    type : '土超德乙西甲',time : '0303 00:08',money : 10,reward : 0,
+    items : [
+        {host : '特拉布宗体育',visiting : '卡拉布克士邦',play : '特拉布宗体育-0.5',odds : 1.825,result : '赢'},
+        {host : '因戈尔施塔特',visiting : '慕尼黑1860',play : '因戈尔施塔特-0.5',odds : 1.45,result : '输'},
+        {host : '塞尔塔',visiting : '艾尔切',play : '特拉布宗体育-0.5',odds : 1.975,result : '赢'},
+    ]
+},{
+    /**************************0303***************************/
+    type : 'NBA滚',time : '0303 10:23',money : 186.87,reward : 356.76,
+    items : [
+        {host : '篮网',visiting : '勇士',play : '小222.5',odds : 1.9,result : '赢'}
     ]
 }];
