68bfec8d1d34e098e39fb5545ec1127f458f6a71
Lich <jy013018@aliyun.com>
2015-03-03 09:52:51 +0800

    更新bet

--- bet/data/2015-3.js
+++ bet/data/2015-3.js
@@ -1,93 +1,39 @@
 var data = [{
-    type : '意甲',
-    time : '0301 22:00',
-    items : [{
-        host : '巴勒莫',
-        visiting : '恩波利',
-        play : '巴勒莫-0.5',
-        odds : 2.3
-    }],
-    money : 30,
-    reward : 0
+    type : '意甲',time : '0301 22:00',money : 30,reward : 0,
+    items : [
+        {host : '巴勒莫',visiting : '恩波利',play : '巴勒莫-0.5',odds : 2.3}
+    ]
 },{
-	type : 'NBA',
-    time : '0301 22:30',
-    items : [{
-        host : '黄蜂',
-        visiting : '魔术',
-        play : '小187.0',
-        odds : 1.9
-    }],
-    money : 30,
-    reward : 57.28
+	type : 'NBA',time : '0301 22:30',money : 30,reward : 57.28,
+    items : [
+        {host : '黄蜂',visiting : '魔术',play : '小187.0',odds : 1.9}
+    ]
 },{
-	type : 'NBA',
-    time : '0301 22:47',
-    items : [{
-        host : '76人',
-        visiting : '步行者',
-        play : '大183.5',
-        odds : 1.47
-    },{
-        host : '鹈鹕',
-        visiting : '掘金',
-        play : '小205.0',
-        odds : 1.47
-    },{
-        host : '黄蜂',
-        visiting : '魔术',
-        play : '小189.0',
-        odds : 1.74
-    }],
-    money : 20,
-    reward : 0
+	type : 'NBA',time : '0301 22:47',money : 20,reward : 0,
+    items : [
+        {host : '76人',visiting : '步行者',play : '大183.5',odds : 1.47},
+        {host : '鹈鹕',visiting : '掘金',play : '小205.0',odds : 1.47},
+        {host : '黄蜂',visiting : '魔术',play : '小189.0',odds : 1.74}
+    ]
 },{
-	type : 'NBA',
-    time : '0302 04:39',
-    items : [{
-        host : '骑士',
-        visiting : '火箭',
-        play : '火箭+1.5',
-        odds : 1.86
-    }],
-    money : 40,
-    reward : 74.79
+	type : 'NBA',time : '0302 04:39',money : 40,reward : 74.79,
+    items : [
+        {host : '骑士',visiting : '火箭',play : '火箭+1.5',odds : 1.86}
+    ]
 },{
-	type : '韩篮甲',
-	time : '0302 11:21',
-	items : [{
-        host : '仁川大象',
-        visiting : '蔚山现代太阳神',
-        play : '大145.0',
-        odds : 1.44
-    },{
-    	host : '昌原猎隼',
-        visiting : '首尔迅雷',
-        play : '大154.5',
-        odds : 1.49
-    }],
-    money : 15,
-    reward : 0
+	type : '韩篮甲',time : '0302 11:21',money : 15,reward : 0,
+    items : [
+        {host : '仁川大象',visiting : '蔚山现代太阳神',play : '大145.0',odds : 1.44},
+        {host : '昌原猎隼',visiting : '首尔迅雷',play : '大154.5',odds : 1.49}
+    ]
 },{
-	type : 'NCAA',
-	time : '0302 11:23',
-	items : [{
-        host : '华盛顿州',
-        visiting : 'UCLA',
-        play : '大143.5',
-        odds : 1.86
-    }],
-    money : 20,
-    reward : 0
+	type : 'NCAA',time : '0302 11:23',money : 20,reward : 0,
+	items : [
+        {host : '华盛顿州',visiting : 'UCLA',play : '大143.5',odds : 1.86}
+    ]
 },{
-	type : '韩篮甲',
-	time : '0302 17:32',
-	items : [{
-        host : '仁川大象',
-        visiting : '蔚山现代太阳神',
-        play : '大151.0',
-        odds : 1.9
-    }],
-    money : 30,
-    reward : 0
+	type : '韩篮甲',time : '0302 17:32',money : 30,reward : 0,
+    items : [
+        {host : '仁川大象',visiting : '蔚山现代太阳神',play : '大151.0',odds : 1.9}
+    ]
 }];
