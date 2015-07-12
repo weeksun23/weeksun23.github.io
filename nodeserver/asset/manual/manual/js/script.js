8fa5623da4f4a81bc1aa6fb71686eac73f47c8d0
Lich <jy013018@aliyun.com>
2015-07-13 00:03:41 +0800

    update

---
+++ nodeserver/asset/manual/manual/js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    var mySwiper = new Swiper('.swiper-container',{
+        paginationClickable: true,
+        slidesPerView: 1,
+        direction: 'vertical',
+        onTouchEnd: function(){
+        	$('.arrow').css('display', 'block');
+   			},
+        onReachEnd: function(){
+     			 $('.arrow').css('display', 'none');
+    		}
+    })
+})
