03394763cff5c566f3d8a1866da24b279df05cce
Lich <jy013018@aliyun.com>
2015-11-26 09:36:36 +0800

    big change

---
+++ js/data.js
@@ -0,0 +1,35 @@
+define(function(){
+	return [{
+		title : 'jQuery',
+		children : [{
+			href : "workspace/jscroll/index.html",title : "jscroll",date : "2015-05-01",
+			desc : "jquery自定义滚动条插件,完善中..."
+		}]
+	},{
+		title : 'avalon',
+		children : [{
+			href : "YuriUI/demo/index.html",title : "Yuri ui",date : "2014-11-21",
+			desc : "基于avalon的ui库"
+		},{
+			href : "AvalonBootstrap",title : "AvalonBootstrap",date : "2015-07-19",
+			desc : "用avalon做的一些bootstrap组件"
+		}]
+	},{
+		title : 'avalon.mobile',
+		children : [{
+			href : "AMUI/demo/pick.html",title : "pick",date : "2015-04-28",desc : "滑动选择插件"
+		},{
+			href : "AMUI/demo/dialog.html",title : "dialog",date : "2015-04-28",desc : "对话框"
+		},{
+			href : "AMUI/demo/slideitem.html",title : "slideitem",date : "2015-04-28",desc : "滑动列表"
+		},{
+			href : "AMUI/demo/slidemenu.html",title : "slidemenu",date : "2015-04-28",
+			desc : "侧边滑动菜单"
+		}]
+	},{
+		title : 'html5',
+		children : [{
+			href : "workspace/clock/clock.html",title : "Clock",date : "2015-02-06",desc : "css3+js的计时时钟"
+		}]
+	}];
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-(function(){
+require(['data'],function(data){
 	var vmodel = avalon.define("weeksun",function(vm){
 		vm.curPage = '';
 		vm.windowHtml = "";
@@ -6,39 +6,7 @@
 		vm.isMenuShow = false;
 		vm.isWindowOpen = false;
 		vm.isMenuAnimateHide = false;
-		vm.labList = [{
-			title : 'jQuery',
-			children : [{
-				href : "jscroll/index.html",title : "jscroll",date : "2015-05-01",
-				desc : "jquery自定义滚动条插件,完善中..."
-			}]
-		},{
-			title : 'avalon',
-			children : [{
-				href : "YuriUI/demo/index.html",title : "Yuri ui",date : "2014-11-21",
-				desc : "基于avalon的ui库"
-			},{
-				href : "AvalonBootstrap",title : "AvalonBootstrap",date : "2015-07-19",
-				desc : "用avalon做的一些bootstrap组件"
-			}]
-		},{
-			title : 'avalon.mobile',
-			children : [{
-				href : "AMUI/demo/pick.html",title : "pick",date : "2015-04-28",desc : "滑动选择插件"
-			},{
-				href : "AMUI/demo/dialog.html",title : "dialog",date : "2015-04-28",desc : "对话框"
-			},{
-				href : "AMUI/demo/slideitem.html",title : "slideitem",date : "2015-04-28",desc : "滑动列表"
-			},{
-				href : "AMUI/demo/slidemenu.html",title : "slidemenu",date : "2015-04-28",
-				desc : "侧边滑动菜单"
-			}]
-		},{
-			title : 'html5',
-			children : [{
-				href : "Clock/clock.html",title : "Clock",date : "2015-02-06",desc : "css3+js的计时时钟"
-			}]
-		}];
+		vm.labList = data;
 		vm.$onClickMask = function(){
 			vmodel.isWindowOpen = false;
 		};
@@ -110,4 +78,4 @@
 		window.onhashchange = dealHash;
 		dealHash();
 	})();
-})();
+});
